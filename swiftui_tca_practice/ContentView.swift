7a678ea2a8483d2b30af04262ec5c290786465a5
sauna1137 <subscription.search@gmail.com>
2024-06-13 07:22:32 +0900

    Initial Commit

---
+++ swiftui_tca_practice/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  swiftui_tca_practice
+//
+//  Created by KS on 2024/06/13.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ swiftui_tca_practice/swiftui_tca_practiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftui_tca_practiceApp.swift
+//  swiftui_tca_practice
+//
+//  Created by KS on 2024/06/13.
+//
+
+import SwiftUI
+
+@main
+struct swiftui_tca_practiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
