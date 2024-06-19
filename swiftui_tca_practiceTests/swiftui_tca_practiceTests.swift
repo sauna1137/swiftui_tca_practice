04d26d285ebabc7a6e2bf0d37b5a274d39e0dbfa
sauna1137 <subscription.search@gmail.com>
2024-06-19 18:39:27 +0900

    Add target of test

---
+++ swiftui_tca_practiceTests/swiftui_tca_practiceTests.swift
@@ -0,0 +1,20 @@
+//
+//  swiftui_tca_practiceTests.swift
+//  swiftui_tca_practiceTests
+//
+//  Created by KS on 2024/06/19.
+//
+
+import ComposableArchitecture
+import XCTest
+
+
+@testable import swiftui_tca_practice
+
+@MainActor
+final class CounterFeatureTests: XCTestCase {
+  func testCounter() async {
+
+  }
+}
+
