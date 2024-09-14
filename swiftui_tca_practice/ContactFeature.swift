15d7b587ce94f8766dadafc8fc60ac2366593507
sauna1137 <subscription.search@gmail.com>
2024-09-14 21:55:27 +0900

    add ContactFeature

---
+++ swiftui_tca_practice/ContactFeature.swift
@@ -0,0 +1,18 @@
+//
+//  ContactFeature.swift
+//  swiftui_tca_practice
+//
+//  Created by KS on 2024/09/13.
+//
+
+import SwiftUI
+
+struct ContactFeature: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ContactFeature()
+}
