43a4bfd714954b4051e5716c9db6a2def7cfff35
sauna1137 <subscription.search@gmail.com>
2024-09-18 07:33:05 +0900

    Navigation section1

---
+++ swiftui_tca_practice/AddContactView.swift
@@ -0,0 +1,18 @@
+//
+//  AddContactView.swift
+//  swiftui_tca_practice
+//
+//  Created by KS on 2024/09/18.
+//
+
+import SwiftUI
+
+struct AddContactView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AddContactView()
+}
